44a896779d7f333a78cd29603fc1296215f4a7bc
noushid <pnoushid@gmail.com>
2018-03-23 10:54:19 +0530

    dlkjlk

---
+++ updation.php
@@ -0,0 +1,16 @@
+<li class="">
+    <a data-toggle="dropdown" class="dropdown-toggle" href="#" style="padding: 5px 10px;min-height: 50px;height: 50px;margin: 0 auto;" aria-expanded="false">
+        <span class="m-r-sm text-muted welcome-message text-xs block" style="font-weight: 400;">Welcome Agency  Manager<b class="caret"></b></span>
+        <span class="block m-t-xs"> <strong class="font-bold">Test Agency e</strong>                    </span>
+
+
+    </a>
+    <ul class="dropdown-menu animated fadeInRight m-t-xs">
+        <li><a href="http://www.oasis-c1.com/boot2/my-profile#tabchange">My Profile</a></li>
+        <li><a onclick="location.reload();" href="http://www.oasis-c1.com/boot2/my-profile#tab-change-password">Change Password</a></li>
+        <li><a onclick="location.reload();" href="http://www.oasis-c1.com/boot2/my-profile#tab-electronic-signature">Electronic Signature</a></li>
+        <li><a onclick="location.reload();" href="http://www.oasis-c1.com/boot2/my-profile#tab-credential">My Credential</a></li>
+        <li class="divider"></li>
+        <li><a onclick="location.reload();" href="http://www.oasis-c1.com/boot2/logout">Logout</a></li>
+    </ul>
+</li>
